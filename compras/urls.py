35290dddcbe6a324b6219d41b0f8930766f6381a
sergiopacheco04 <sergio.pacheco@ucsm.edu.pe>
2024-05-02 13:59:08 -0500

    endpoint Dashboard

--- compras/urls.py
+++ compras/urls.py
@@ -4,4 +4,5 @@ from .views import Inicio,CrearOrden
 urlpatterns = [
     path('', Inicio.as_view(), name='bienvenida'),
     path('crear-orden/', CrearOrden.as_view(), name='crear-orden'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
 ]
