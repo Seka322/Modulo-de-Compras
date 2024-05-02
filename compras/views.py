c3a349e1c2b5093cd0dc622c260076598fe1fc7d
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-02 13:10:22 -0500

    Fix Vista Orden de Compra

--- compras/views.py
+++ compras/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .forms import CompraForm
