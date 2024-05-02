9f4d79c86b4650eaebdfc68d3a0768e9f8765019
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-02 12:36:00 -0500

    Crear tabla ItemCompra

--- compras/models.py
+++ compras/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ItemCompra(models.Model):
+	nombre = models.CharField(max_length=200)
+	cantidad = models.IntegerField()
+	categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, blank=True, null=True)
+	fecha_creacion = models.DateTimeField(auto_now_add=True)
+	usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+	sucursal = models.ForeignKey('Sucursal', on_delete=models.SET_NULL, blank=True, null=True)
+	
+	def __str__(self):
+		return self.nombre
