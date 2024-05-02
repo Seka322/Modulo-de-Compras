bb05acc4b6988ca34496312729464dc68f0e31ba
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-02 12:31:15 -0500

    Creación formulario de Compra

---
+++ compras/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Sucursal, Categoria, ItemCompra
+
+class CompraForm(forms.ModelForm):
+	categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), initial=0)
+	sucursal = forms.ModelChoiceField(queryset=Sucursal.objects.all(), initial=0)
