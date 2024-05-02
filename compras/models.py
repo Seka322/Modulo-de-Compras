ec55ff4a837e69fc02b5d54b0237d5bff8a59cca
sergiopacheco04 <sergio.pacheco@ucsm.edu.pe>
2024-05-02 13:58:13 -0500

    crear_tabla_item

--- compras/models.py
+++ compras/models.py
@@ -27,3 +27,15 @@ class Sucursal(models.Model):
 
 	def __str__(self):
 		return self.nombre
+	
+class ItemCompra(models.Model):
+	nombre = models.CharField(max_length=200)
+	cantidad = models.IntegerField()
+	categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, blank=True, null=True)
+	fecha_creacion = models.DateTimeField(auto_now_add=True)
+	usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+	sucursal = models.ForeignKey('Sucursal', on_delete=models.SET_NULL, blank=True, null=True)
+	
+	def __str__(self):
+		return self.nombre
+	
