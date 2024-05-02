0b6cd6a97344700529a9f0077f5c430c8df7db26
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-02 12:32:33 -0500

    Detalles del Orden de Compra

--- compras/forms.py
+++ compras/forms.py
@@ -4,3 +4,6 @@ from .models import Sucursal, Categoria, ItemCompra
 class CompraForm(forms.ModelForm):
 	categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), initial=0)
 	sucursal = forms.ModelChoiceField(queryset=Sucursal.objects.all(), initial=0)
+	class Meta:
+		model = ItemCompra
+		fields = ['nombre', 'cantidad', 'categoria', 'sucursal']
