043d7dba64b9dec0f9668336eb0d03e2b4f9e982
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-02 12:38:08 -0500

    Crear tabla Sucursal

--- compras/models.py
+++ compras/models.py
@@ -20,3 +20,10 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+	
+class Sucursal(models.Model):
+	nombre = models.CharField(max_length=200)
+	direccion = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.nombre
