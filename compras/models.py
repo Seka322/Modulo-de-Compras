fa28a7ccf33d09345a73c092b79152a2c8bf0487
Joel <armando.guia@ucsm.edu.pe>
2024-05-09 14:52:37 -0500

    Cambios en el modelo de ItemCompra

--- compras/models.py
+++ compras/models.py
@@ -7,6 +7,7 @@ class ItemCompra(models.Model):
 	unidades = models.IntegerField()
 	minimo_unidades = models.IntegerField()
 	costo = models.DecimalField(max_digits=10, decimal_places=2)
+	costo_total = models.DecimalField(max_digits=10, decimal_places=2)
 	cantidad = models.IntegerField()
 	fecha_orden = models.DateTimeField(auto_now_add=True)
 	fecha_entrega = models.DateTimeField()
