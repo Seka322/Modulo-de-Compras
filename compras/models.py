7cf5233c4f89957f62dd117637258e9ce633397a
Seka322 <josue.challa@ucsm.edu.pe>
2024-05-09 13:21:17 -0500

    Agregar atributos de l Modelo Item de Compra

--- compras/models.py
+++ compras/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ItemCompra(models.Model):
-	
-	cantidad = models.IntegerField()
 	sucursal = models.ForeignKey('Sucursal', on_delete=models.SET_NULL, blank=True, null=True)
-	
+	producto = models.CharField(max_length=255)
+	proveedor = models.CharField(max_length=255)
+	unidades = models.IntegerField()
+	minimo_unidades = models.IntegerField()
+	costo = models.DecimalField(max_digits=10, decimal_places=2)
+	cantidad = models.IntegerField()
+	fecha_orden = models.DateTimeField(auto_now_add=True)
+	fecha_entrega = models.DateTimeField()
+
 	def __str__(self):
 		return self.nombre
 	
