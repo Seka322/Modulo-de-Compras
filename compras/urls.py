7575c58e4eb23bad1856dbb8780f1debc9b44260
Joel <joel.pinto@ucsm.edu.pe>
2024-05-09 13:57:34 -0500

    Endpoint cerrar sesion

--- compras/urls.py
+++ compras/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('iniciar-sesion/', auth_views.LoginView.as_view(template_name='login.html'), name='iniciar-sesion'),
     path('registro/', Registro.as_view(), name='registro'),
+     path('cerrar-sesion/', auth_views.LogoutView.as_view(template_name='logout.html'), name='cerrar-sesion'),
 ]
