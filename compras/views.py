dd11b12b952698e61f173105718e3a40f3751da8
Joel <sergio.pacheco@ucsm.edu.pe>
2024-05-09 14:23:53 -0500

    Fix Import para vista de Eliminacion

--- compras/views.py
+++ compras/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import View, CreateView, TemplateView
+from django.views.generic import View, CreateView, TemplateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .forms import CompraForm, RegistroForm
