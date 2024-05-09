7b8977a4f635cd676f22d1fcd01df4023badedb8
Joel <sergio.pacheco@ucsm.edu.pe>
2024-05-09 15:14:57 -0500

    Nueva db de ejmplo +height

--- db.py
+++ db.py
@@ -11,15 +11,15 @@ ejemplos_sucursales = [
         ('Mini Market Doña Rosa', 'Calle Los Algarrobos 345, Piura')
 ]
 ejemplos_itemproveedor = [
-        ('Arroz Doria 1 kg', 'Doria Distribuciones', '1 kg', 102, 10, 2.50),
-        ('Azúcar Blanca Inca 1 kg', 'Inca Azúcares', '1 kg', 52, 10, 3.20),
-        ('Aceite Girasol Primor 1 litro', 'Aceites Primor', '1 litro', 186, 10, 5.50),
-        ('Sal Marasal 500 g', 'Marasal SA', '500 g', 342, 10, 1.80),
-        ('Fideos Tallarín Don Vicente 500 g', 'Fideos Don Vicente', '500 g', 95, 10, 2.00)
+        ('Arroz Doria 1 kg', 'Doria Distribuciones', 102, 5, 2.50),
+        ('Azúcar Blanca Inca 1 kg', 'Inca Azúcares', 52, 3, 3.20),
+        ('Aceite Girasol Primor 1 litro', 'Aceites Primor', 186, 7, 5.50),
+        ('Sal Marasal 500 g', 'Marasal SA', 342, 10, 1.80),
+        ('Fideos Tallarín Don Vicente 500 g', 'Fideos Don Vicente', 95, 5, 2.00)
     ]
 
 for item in ejemplos_itemproveedor:
-    cursor.execute("INSERT INTO compras_itemproveedor (item, proveedor, cantidad_por_unidad, unidades, minimo_unidades, costo) VALUES (?, ?, ?, ?, ?, ?)", item)
+    cursor.execute("INSERT INTO compras_itemproveedor (item, proveedor, unidades, minimo_unidades, costo) VALUES (?, ?, ?, ?, ?)", item)
 
 for sucursal in ejemplos_sucursales:
     cursor.execute("INSERT INTO compras_sucursal (nombre, direccion) VALUES (?, ?)", sucursal)
