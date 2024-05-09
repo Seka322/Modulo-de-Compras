cf83357fd0280a716ec2cf6ce9e1bbfeefc6158a
Joel <sergio.pacheco@ucsm.edu.pe>
2024-05-09 14:30:43 -0500

    Fix Import sin uso

--- compras/models.py
+++ compras/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class ItemCompra(models.Model):
 	sucursal = models.ForeignKey('Sucursal', on_delete=models.SET_NULL, blank=True, null=True)
